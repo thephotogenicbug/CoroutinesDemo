b68b391f5fd2dd49a20caa70688b5bb745da99cc
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-11 14:16:40 +0530

    Finalize Coroutines

--- app/src/main/java/com/naveen/coroutinesdemo/MainActivity.kt
+++ app/src/main/java/com/naveen/coroutinesdemo/MainActivity.kt
@@ -5,17 +5,16 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
+    private lateinit var messageTextView : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val textView = findViewById<TextView>(R.id.tvCount)
+        messageTextView = findViewById(R.id.tvMessage)
         val countButton = findViewById<Button>(R.id.btnCount)
         val downloadButton = findViewById<Button>(R.id.btnDownload)
 
@@ -32,6 +31,10 @@ class MainActivity : AppCompatActivity() {
     private suspend fun downloadUserData() {
         for (i in 1..200000) {
             Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main){
+                messageTextView.text = "Downloading user $i"
+            }
+
             delay(100)
         }
     }
